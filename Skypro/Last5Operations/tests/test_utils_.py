c65269d5c403ed357d16c4ca700375bc5b2e8e69
Стас naprienko ekaterina <catrit1993@gmail.com>
2023-12-04 17:37:24 +0500

    add functions and tests

--- Skypro/Last5Operations/tests/test_utils_.py
+++ Skypro/Last5Operations/tests/test_utils_.py
@@ -1,5 +1,99 @@
-from utils.utils_ import get_from_json
+import pytest
+from tests.conftest import utils_fix
+
+from utils.utils_ import read_json, search_executed, sort_executed, edit_date, secret_numb
 
 
-def test_get_from_json():
-    assert get_from_json()[0]['id'] == 441945886
+def test_read_json():
+    operations = read_json()
+    assert read_json()[0]['id'] == 441945886
+    assert read_json()[0]['date'] == "2019-08-26T10:50:58.294041"
+    assert len(operations) == 101
+
+def test_search_executed(utils_fix):
+    assert search_executed(utils_fix) == [{
+        "id": 716496732,
+        "state": "EXECUTED",
+        "date": "2018-04-04T17:33:34.701093",
+        "operationAmount": {
+            "amount": "40701.91",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Gold 5999414228426353",
+        "to": "Счет 72731966109147704472"
+    },
+    {
+        "id": 863064926,
+        "state": "EXECUTED",
+        "date": "2019-12-08T22:46:21.935582",
+        "operationAmount": {
+            "amount": "41096.24",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 90424923579946435907"
+    }]
+
+
+def test_edit_date():
+    assert edit_date('2018-04-14T19:35:28.978265') == '14.04.2018'
+    assert edit_date('2018-09-12T21:27:25.241689') == '12.09.2018'
+
+def test_secret_numb():
+    assert secret_numb('3152479541115065') == '3152 47** **** 5065'
+    assert secret_numb('3152479541115065', to=True) == '** 5065'
+
+
+def test_sort_executed(utils_fix):
+    assert sort_executed(utils_fix) == [{
+    "id": 863064926,
+    "state": "EXECUTED",
+    "date": "2019-12-08T22:46:21.935582",
+    "operationAmount": {
+      "amount": "41096.24",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 90424923579946435907"
+    },
+    {
+    "id": 594226727,
+    "state": "CANCELED",
+    "date": "2018-09-12T21:27:25.241689",
+    "operationAmount": {
+        "amount": "67314.70",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Platinum 1246377376343588",
+    "to": "Счет 14211924144426031657"
+    },
+        {
+            "id": 716496732,
+            "state": "EXECUTED",
+            "date": "2018-04-04T17:33:34.701093",
+            "operationAmount": {
+                "amount": "40701.91",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Visa Gold 5999414228426353",
+            "to": "Счет 72731966109147704472"
+        }
+    ]
--- Skypro/Last5Operations/utils/utils_.py
+++ Skypro/Last5Operations/utils/utils_.py
@@ -1,8 +1,34 @@
 import json
+import datetime as dt
 
-def get_from_json():
-    with open("operations.json", "r") as file:
-        operations_json = file.read()
 
-    return json.loads(operations_json)
+def read_json():
+    '''Перевод из json в формат python'''
+    with open("C:/Users/Skypro/Last5Operations/operations.json", "r", encoding="utf-8") as file:
+        return json.load(file)
+
+def search_executed(data):
+    """Выборка успешных операций"""
+    data = [elem for elem in data if elem and elem['state'] == 'EXECUTED']
+    return data
+
+def sort_executed(data):
+    s_data = sorted(data, key=lambda x:x['date'], reverse=True)
+    return s_data[:5]
+
+def edit_date(date:str):
+    date = date[0:10]
+    date = dt.datetime.strptime(date, "%Y-%m-%d")
+    new_date = date.strftime("%d.%m.%Y")
+    return new_date
+
+def secret_numb(number:str, to=False): #для карты, для счета указать тру
+    if not to:
+        return f'{number[:4]} {number[4:6]}** **** {number[-4:]}'
+    else:
+        return f'** {number[-4:]}'
+
+
+def print_operations():
+    pass
 
